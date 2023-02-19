19e8aa6d58cf654b2af029624f238f01f474edea
MuhmmadAbrar <88892675+MuhmmadAbrar@users.noreply.github.com>
2023-02-19 18:41:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,6 +45,16 @@ def predictor():
     st.write('## This model predicts if a given scanned image of a brain results Azheimer positive or not.')
     st.write('##### Note: Our model can access real time images too. But inorder to get accurate results, please upload a softcopy.')
 
+    st.write("# ")
+    st.write("## Alzheimer's disease is a type of brain disease caused by damage to nerve cells (neurons) in the brain.")
+    st.write("Alzheimer's disease is the most common type of dementia. It is a progressive disease beginning with mild memory loss and possibly leading to loss of the ability to carry on a conversation and respond to the environment. Alzheimer's disease involves parts of the brain that control thought, memory, and language.The neurons damaged first are those in parts of the brain responsible for memory, language and thinking. As a result, the first symptoms of Alzheimer’s disease tend to be memory, language and thinking problems.")
+    st.write("")
+    st.write("#### Check if you are affected by alzheimer's with our website!!")
+    st.write("#### This can be done by a single click of the mouse and 1 image upload")
+    res = st.button("Click here to open our Alzeimer Predictor")
+    if res:
+        predictor()
+
     model = tf.keras.models.load_model("customneural.h5")
     image = 0
 
