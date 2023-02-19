6d3fdff7e9cf82f924c38cbd81faee1059cc510f
MuhmmadAbrar <88892675+MuhmmadAbrar@users.noreply.github.com>
2023-02-19 18:38:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -102,7 +102,4 @@ page_names_to_funcs = {
     "Click to know about Azheimer": about_azheimer, 
 }
 
-selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
-page_names_to_funcs[selected_page]()
-
 
